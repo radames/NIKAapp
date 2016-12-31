37e8f5ce6a034d245574ab3375f03598357a20bc
Radames Ajna <radamajna@gmail.com>
2016-12-31 19:41:53 +0100

    different colours for working groups

--- public/js/graph.js
+++ public/js/graph.js
@@ -3,11 +3,36 @@ var container = document.getElementById('visualization');
 
 // Create a DataSet (allows two way data-binding)
 
+
+//sprintf like
+//from http://www.kisphp.com/javascript/javascript-sprintf-function
+String.prototype.format = function(){
+    // get function arguments
+    var args = arguments;
+
+    // replace variables in string
+    return this.replace(/{(\d+)}/g, function(match, index){
+
+        // return replaced variable
+        return args[index];
+    });
+};
 // Configuration for the Timeline
 var options = {
   width: '100%',
-  height: 300, // px
-  orientation: {axis: 'top', item: 'top'}
+  height: 450, // px
+  orientation: {axis: 'top', item: 'top'},
+  template: function (item) {
+  var groupList = item.className.trim().split(' ');
+  var html = '';
+  var div = '<div id="{0}">{1}</div>';
+
+  groupList.forEach((e, i) => {
+
+      html += i > 0? div.format(e,'&nbsp;'):div.format(e, item.content);
+    });
+  return html;
+ }
 };
 
 // Create a Timeline
