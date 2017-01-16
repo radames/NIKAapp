82261cfba021165a5e3bd7048f18f95ae44e49f3
Radames Ajna <radamajna@gmail.com>
2017-01-16 16:07:51 +0000

    better timeline start range

--- public/js/timeline.js
+++ public/js/timeline.js
@@ -17,16 +17,23 @@ String.prototype.format = function(){
     return args[index];
   });
 };
+
 // Configuration for the Timeline
+//start window
+var startWindowX = new Date();
+var endWindowX =  new Date();
+endWindowX.setDate(endWindowX.getDate() + 30); //30 days from now
+
 var options = {
   width: '100%',
   height: 450, // px
   orientation: {axis: 'top', item: 'top'},
   min: new Date(2016, 11, 1),                // lower limit of visible range
-  max: new Date(2018, 12, 1),                // upper limit of visible range
   zoomMin: 1000 * 60 * 60 * 24,             // one day in milliseconds
   zoomMax: 1000 * 60 * 60 * 24 * 31 * 12,    // about three months in milliseconds
-
+  showCurrentTime: true,
+  start: startWindowX,
+  end: endWindowX,
 
   template: function (item) {
     var groupList = item.className.trim().split(' ');
@@ -45,11 +52,12 @@ var options = {
 
 var timeline = new vis.Timeline(container, items, options);
 
+
 container.addEventListener('mouseover', mouseEvent);
 container.addEventListener('mousemove', mouseEvent);
 container.addEventListener('mouseout', mouseEvent);
 container.addEventListener('dblclick', function(){
-  timeline.fit();
+  timeline.setWindow(startWindowX, endWindowX);
 });
 
 function mouseEvent(e) {
