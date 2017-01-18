8add4ac64fa6c9aa9990d1e0f7218bf58c485376
Radames Ajna <radamajna@gmail.com>
2017-01-18 16:42:01 +0000

    added tooltip to graph nodes

--- public/js/graph.js
+++ public/js/graph.js
@@ -24,22 +24,62 @@ var options = {
   width: '100%',
   height: '450', // px
   layout: { hierarchical: { sortMethod: 'directed', direction: 'LR' }
+},
+interaction:{
+  hover: true
+},
+nodes: {
+  shape: 'dot',
+  size: 20,
+  font: {
+    size: 17
   },
-  nodes: {
-    shape: 'dot',
-    size: 20,
-    font: {
-      size: 17
-    },
-    borderWidth: 2,
-    shadow:true
-  },
-  edges: {
-    width: 2,
-    shadow:true
-  }
+  borderWidth: 2,
+  shadow:true
+},
+edges: {
+  width: 2,
+  shadow:true
+}
 };
 var network = new vis.Network(container, data, options);
 container.addEventListener('dblclick', function(){
   network.fit();
 });
+
+
+
+
+network.on("blurNode", function(params){
+  mouseEvent('out', params);
+  container.removeEventListener('mousemove', function(){
+    mouseEvent('mousemove', event)
+  });
+});
+network.on("hoverNode", function(params){
+  mouseEvent('in', params);
+  container.addEventListener('mousemove', function(){
+    mouseEvent('mousemove', event)
+  });
+});
+
+var actualNode;
+function mouseEvent(e, event) {
+  console.log(e,event);
+  switch(e){
+  case 'in':
+    actualNode = document.getElementById(event.node);
+    actualNode.classList.add('active');
+    break;
+  case 'out':
+    actualNode = document.getElementById(event.node);
+    actualNode.classList.remove('active');
+    break;
+  case 'mousemove':
+    var x = event.clientX;
+    var y = event.clientY;
+    actualNode.style.top = (y + 5) + 'px';
+    actualNode.style.left = (x + 5) + 'px';
+    break;
+  }
+};
