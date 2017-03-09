4c9378ac654e906e9ae6ff61e03699d6a6b69226
Radames Ajna <radamajna@gmail.com>
2017-03-09 11:59:18 +0000

    adding subtitle field

--- models/Task.js
+++ models/Task.js
@@ -14,13 +14,13 @@ var Task = new keystone.List('Task',{
 
 Task.add({
   title: { type: String, initial: true, default: '', required: true ,  label:'Task Title' ,},
+  subtitle: { type: String, initial: true, default: '', required: true ,  label:'Task Subtitle' ,},
   description: { type: Types.Markdown, wysiwyg: true,  height: 200 , label:'Task Description',},
   assignedTo: { type: Types.Relationship, ref: 'User', index: true, many: true, label:'Assigned To'},
   workingGroup: { type: Types.Relationship, ref: 'WorkingGroup', index: true, many: true, label:'Working Group',},
   startOn: { type: Types.Datetime, default: Date.now, label: 'Task starts on (HH:MM:SS am/pm)'},
   endOn: { type: Types.Datetime, default: Date.now, label: 'Task ends on (HH:MM:SS am/pm)' },
   regularEvent: { type: Types.Boolean, label: 'Regular Event' }
-
 });
-Task.defaultColumns = 'title, workingGroup, startOn, endOn, createdBy';
+Task.defaultColumns = 'title, subtitle, workingGroup, startOn, endOn, createdBy';
 Task.register();
