33457629cc840442e03cfce47cdc5bce58ce4183
Radames Ajna <radamajna@gmail.com>
2017-03-19 11:54:37 -0300

    fix working groups nodes hovermessage

--- public/js/graph.js
+++ public/js/graph.js
@@ -22,88 +22,95 @@ var initTaskMap = function(data){
   //   edges: relations
   // };
   var options = {
-    width: '100%',
-    height: '450', // px
-    layout: { hierarchical: { sortMethod: 'directed', direction: 'LR' }
-  },
-  interaction:{
-    hover: true
-  },
-  nodes: {
-    shape: 'dot',
-    size: 20,
-    font: {
-      size: 17
+      width: '100%',
+      height: '450', // px
+      layout: { hierarchical: { sortMethod: 'directed', direction: 'LR' }
     },
-    borderWidth: 2,
-    shadow:true
-  },
-  edges: {
-    width: 2,
-    shadow:true
-  },
-  physics: {
-    stabilization: true,
-    hierarchicalRepulsion: {
-      centralGravity: 0.0,
-      springLength: 150,
-      springConstant: 0.01,
-      nodeDistance: 200,
-      damping: 0.09
+    interaction:{
+      hover: true
     },
-  },
-};
-var network = new vis.Network(container, data, options);
-container.addEventListener('dblclick', function(){
-  network.fit();
-});
-
-
-
-
-network.on("blurNode", function(params){
-  mouseEvent('out', params);
-  container.removeEventListener('mousemove', function(event){
-    mouseEvent('mousemove', event)
+    nodes: {
+      shape: 'dot',
+      size: 20,
+      font: {
+        size: 17
+      },
+      borderWidth: 2,
+      shadow:true
+    },
+    edges: {
+      width: 2,
+      shadow:true
+    },
+    physics: {
+      stabilization: true,
+      hierarchicalRepulsion: {
+        centralGravity: 0.0,
+        springLength: 150,
+        springConstant: 0.01,
+        nodeDistance: 200,
+        damping: 0.09
+      },
+    },
+  };
+  var network = new vis.Network(container, data, options);
+  var maxScale = 1;
+  container.addEventListener('dblclick', function(){
+    network.fit();
   });
-});
-network.on("hoverNode", function(params){
-  mouseEvent('in', params);
-  container.addEventListener('mousemove', function(event){
-    mouseEvent('mousemove', event)
+
+  network.on("stabilized", function (params) {
+    maxScale = network.getScale();
   });
-});
-network.on( 'click', function(params) {
-  //redirect to edit tak
-  var nodeID = params.nodes[0];
-  if(nodeID){
-    var el = document.getElementById(nodeID);
-    //check if is a workinggroup node
-    if(!el.getAttribute("data")){
-      window.location.href = window.location.origin + "/admin/tasks/" + nodeID;
+  network.on("zoom", function (params) {
+    console.log(params.scale);
+  });
+  network.on("blurNode", function(params){
+    console.log(params);
+    mouseEvent('out', params);
+    container.removeEventListener('mousemove', function(event){
+      mouseEvent('mousemove', event)
+    });
+  });
+  network.on("hoverNode", function(params){
+    mouseEvent('in', params);
+    container.addEventListener('mousemove', function(event){
+      mouseEvent('mousemove', event)
+    });
+  });
+  network.on("click", function(params) {
+    //redirect to edit tak
+    var nodeID = params.nodes[0];
+    if(nodeID){
+      var el = document.getElementById(nodeID);
+      //check if is a workinggroup node
+      if(!el.getAttribute("data")){
+        window.location.href = window.location.origin + "/admin/tasks/" + nodeID;
+      }
     }
-  }
-});
+  });
 
-var actualNode;
-function mouseEvent(e, event) {
-  switch(e){
-    case 'in':
-    actualNode = document.getElementById(event.node);
-    actualNode.classList.add('active');
-    break;
-    case 'out':
-    actualNode = document.getElementById(event.node);
-    actualNode.classList.remove('active');
-    break;
-    case 'mousemove':
-    var x = event.clientX;
-    var y = event.clientY;
-    actualNode.style.top = (y + 5) + 'px';
-    actualNode.style.left = (x + 5) + 'px';
-    break;
-  }
-};
+  var actualNode;
+  function mouseEvent(e, event) {
+    switch(e){
+      case 'in':
+      actualNode = document.getElementById(event.node);
+      if(actualNode) actualNode.classList.add('active');
+      break;
+      case 'out':
+      actualNode = document.getElementById(event.node);
+      if(actualNode) actualNode.classList.remove('active');
+      break;
+      case 'mousemove':
+      var x = event.clientX;
+      var y = event.clientY;
+      if(actualNode){
+        actualNode.style.top = (y + 5) + 'px';
+        actualNode.style.left = (x + 5) + 'px';
+      }
+      break;
+    }
+  };
 };
 var loadTaskMap = function(wGroupPath){
   console.log('/api/graph/' + wGroupPath);
