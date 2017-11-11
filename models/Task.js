3a154f74635dd4778a56e3e861155b3ae0c90eec
Radames Ajna <radamajna@gmail.com>
2017-11-11 15:26:16 -0800

    fields for email notifications optinons

--- models/Task.js
+++ models/Task.js
@@ -11,7 +11,6 @@ var Task = new keystone.List('Task',{
   singular: 'Task',
   plural: 'Tasks'
 });
-
 Task.add({
   title: { type: String, initial: true, default: '', required: true ,  label:'Task Title' ,},
   subtitle: { type: String, initial: true, default: '', required: true ,  label:'Task Subtitle' ,},
@@ -21,6 +20,28 @@ Task.add({
   startOn: { type: Types.Datetime, default: Date.now, label: 'Task starts on (HH:MM:SS am/pm)'},
   endOn: { type: Types.Datetime, default: Date.now, label: 'Task ends on (HH:MM:SS am/pm)' },
   regularEvent: { type: Types.Boolean, label: 'Regular Event' }
+},'Email Notifications',
+  { emailNotificaionsOn: { type: Types.Boolean, label: "Enable Email Notifications" },
+    notificationPeriods: { label: "Notifications Period", type: Types.TextArray, dependsOn : {emailNotificaionsOn: true}, default: ["1 Week", "1 Day"]}
 });
 Task.defaultColumns = 'title, subtitle, workingGroup, startOn, endOn, createdBy';
+
+Task.schema.methods.isSchedulerOn = function() {
+	return this.emailNotificaionsOn;
+}
+
+Task.schema.pre('save', function(next) {
+	if (this.notificationPeriods.length < 10) {
+    console.log(this.notificationPeriods);
+		var err = new Error('The only valid Time units are units of, Hours, Days and Weeks');
+		next(err);
+	}
+	next();
+});
+
+Task.schema.post('save', function(next) {
+	console.log(this.isSchedulerOn(), this.startOn, this.endOn);
+	next();
+});
+
 Task.register();
